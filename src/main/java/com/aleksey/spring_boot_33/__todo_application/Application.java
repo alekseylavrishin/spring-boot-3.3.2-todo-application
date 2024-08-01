a0365b7b31f76a0e3d6d55a576ce18a11a05c449
alekseylavrishin <78001914+alekseylavrishin@users.noreply.github.com>
2024-08-01 11:05:18 -0400

    Initial commit

---
+++ src/main/java/com/aleksey/spring_boot_33/__todo_application/Application.java
@@ -0,0 +1,13 @@
+package com.aleksey.spring_boot_33.__todo_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/aleksey/spring_boot_33/__todo_application/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aleksey.spring_boot_33.__todo_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
