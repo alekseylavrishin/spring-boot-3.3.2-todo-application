4427730795273e2645215fa7e761178187835bee
alekseylavrishin <78001914+alekseylavrishin@users.noreply.github.com>
2024-08-01 16:32:57 -0400

    Created POST call for creating new list item

--- src/main/java/com/aleksey/spring_boot_33/__todo_application/controllers/ToDoFormController.java
+++ src/main/java/com/aleksey/spring_boot_33/__todo_application/controllers/ToDoFormController.java
@@ -1,9 +1,15 @@
 package com.aleksey.spring_boot_33.__todo_application.controllers;
 
+import ch.qos.logback.core.model.Model;
+import com.aleksey.spring_boot_33.__todo_application.models.ToDoItem;
 import com.aleksey.spring_boot_33.__todo_application.services.ToDoItemService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ToDoFormController {
@@ -17,7 +23,20 @@ public class ToDoFormController {
      * @return Name of view that is returned
      */
     @GetMapping("/create-todo")
-    public String showCreateForm() {
+    public String showCreateForm(ToDoItem item) {
         return "new-todo-item";
+    }
+
+    /**
+     * Listens for the POST call
+     */
+    @PostMapping("/todo")
+    public String createToDoItem(@Valid ToDoItem item, BindingResult result, Model model) {
+        ToDoItem TDItem = new ToDoItem();
+        TDItem.setDescription(item.getDescription());
+        TDItem.setComplete(item.isComplete());
+
+        service.save(item);
+        return "redirect:/";
     }
 }
